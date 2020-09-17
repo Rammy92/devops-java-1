970c2457ff0391fc4f52e9c0b22d51fd4d0699e6
Rammy92 <rammy.mansour@utb.ecutbildning.se>
2020-09-17 11:13:24 +0200

    game

---
+++ game.java/src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class Main {
+
+    String characterName;
+    String weaponName;
+    int weaponDamage;
+    int hp;
+    static int hammerMaximumDamage = 50;
+    static int hammerMinimumDamage = 10;
+
+    static int hammer = (int) (Math.random() * ((hammerMaximumDamage - hammerMinimumDamage) + 1)) + hammerMinimumDamage;
+    static int magicWandMaximumDamage = 100;
+    static int magicWandMinimumDamage = 0;
+    static int magicWand = (int) (Math.random() * (magicWandMaximumDamage - magicWandMinimumDamage));
+
+    public Main(String charName, int wDamage, int health, String wName) {
+        characterName = charName;
+        weaponName = wName;
+        weaponDamage = wDamage;
+        hp = health;
+    }
+
+    public void beingAttacked(String defenderName, String attackerName, int attackerWeaponDamage, String attackerWeaponName) {
+        if (hp <= 0 || attackerWeaponName == "Excalibur") {
+            System.err.println("YOU CANNOT ATTACK");
+        } else {
+            /* System.err.println("WILD " + attackerName + " APPEARS!"); */
+            System.out.println(attackerName + " STRIKES " + defenderName + " WITH A " + attackerWeaponName + " IT DOES " + attackerWeaponDamage + " DAMAGE");
+            hp = (hp - attackerWeaponDamage);
+            System.out.println(defenderName + " HAS " + hp + " REMAINING HEALTH! ");
+        }
+    }
+
+    public static void searchWeapon(String weaponName) {
+        switch(weaponName) {
+            case "Hammer":
+                System.out.println("The weapon that Good guy use is Hammer. " );
+                break;
+            case "Magic Wand":
+                System.out.println("The weapon that Evil guy use is Magic Wand. ");
+                break;
+            default:
+                System.err.println("Cant find a hero that use this weapon");
+                break;
+        }
+    }
+
+    public void changeWeapon() {
+        weaponName = "Excalibur";
+        System.out.print("The evil guy has changed his weapon to " + weaponName);
+
+    }
+
+    public void drinkingHealingPotion() {
+        int healingPotion = 20;
+        hp = hp + healingPotion;
+        System.out.println(characterName + " GAINED " + healingPotion + " HEALTH BY DRINKING A HEALING POTION");
+        System.out.println(characterName + " NOW HAS A HEALTH OF " + hp);
+    }
+
+
+
+    public static void main(String[] args) {
+        Main goodGuy = new Main("The Beast", hammer, 100, "Hammer");
+        Main evilGuy = new Main("Ja'far", magicWand, 80, "Magic Wand");
+
+        goodGuy.beingAttacked(goodGuy.characterName, evilGuy.characterName, evilGuy.weaponDamage, evilGuy.weaponName);
+        evilGuy.beingAttacked(evilGuy.characterName, goodGuy.characterName, goodGuy.weaponDamage, goodGuy.weaponName );
+        goodGuy.drinkingHealingPotion();
+        evilGuy.drinkingHealingPotion();
+        searchWeapon("Hammer");
+        evilGuy.changeWeapon();
+    }
+}
